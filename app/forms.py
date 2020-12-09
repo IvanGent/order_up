38bb1b71173ff60d28cec17e8093ad526ca8e421
Philip <philipling83@gmail.com>
2020-12-09 17:16:54 -0500

    Handle login and logout routes

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+  employee_number = StringField("Employee number", [DataRequired()])
+  password = PasswordField("Password", [DataRequired()])
+  submit = SubmitField("Login")
